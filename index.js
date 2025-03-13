a6cf392374943819e5cc120ac22a52e8169974ec
Satya Jakkampudi <satyajakkampudi4@gmail.com>
2025-03-13 14:34:35 +0530

    Built an CRUD API for users data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const users = require("./MOCK_DATA.json");
 const fs = require('fs');
-const { Console } = require('console');
+
 
 const app = express();
 dotenv.config();
@@ -21,6 +21,8 @@ const PORT = process.env.PORT;
 // });
 
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 
 //RESTAPI
 app.get('/api/users',(req,res)=>{
@@ -35,21 +37,49 @@ app
     return res.json(users.find((user) => user.id === id)); 
 })
 .put((req,res)=>{
-    //TODO : update user by id
-    return res.json({msg:"Status pending"})
+    // Update user by id
+    const id = Number(req.params.id);
+    const body = req.body;
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+        return res.status(404).json({msg: "User not found"});
+    }
+
+    users[userIndex] = { ...users[userIndex], ...body };
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        if (err) {
+            return res.status(500).json({msg: "Server Error"});
+        }
+        return res.status(200).json({msg: "Updated Successfully"});
+    });
 })
+
+
 .delete((req,res)=>{
     //TODO : delete user by id
-    return res.json({msg:"Status pending"})
+    const id = Number(req.params.id);
+    const newUserList = users.filter((user)=>user.id !== id)
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(newUserList), (err)=>{
+        if(err){
+            return res.status(500).json({msg : "Server Error"})
+        }
+        return res.status(200).json({msg: "Deleted Successfully"})
+        console.log("Deleted Succesfully")
+    })
 })
 
 app.post('/api/users',(req,res)=>{
     //TODO : create user by id
     const body = req.body;
-    users.push({id:users.length+1 ,...body});
+    users.push({id:users.length+1, ...body});
     fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        if(err){
+            return res.status(500).json({msg : "Server Error"})
+        }
+        return res.status(200).json({msg: "Posted Successfully"})
         console.log("New user inserted!");
     });  
+    
 });
 
 
